5d3fcd3a88f5a5590abd844b374c4b1e2166d468
Scott Spicer <scottspicer24@gmail.com>
2025-03-28 12:52:51 -0400

    XCLIP inference fixed

--- confirm_actions.py
+++ confirm_actions.py
@@ -42,17 +42,20 @@ def subsample_frames(frames_list):
     indices = np.linspace(0, len(frames_list) - 1, num=32, dtype=int)
     return [frames_list[i] for i in indices]   
 
-def inference(model, processor, video, action_desc, threshold):
+def inference(model, processor, video, action_desc, idx, print_scores):
     """ 
-    TODO: write this
-    sumary_line
+    Performs inference with the xclip model and returns if the action is being performed or not.
     
-    Keyword arguments:
-    argument -- description
-    Return: return_description
+    Args:
+        Model (Hugging Face model): The XCLIP model.
+        processor (Hugging Face processor): The XCLIP Processor.
+        action_desc (list) : A list of strings of action descriptions.
+        idx (int) : The index in action_desc of the description representing the action in question.
+        print_scores (bool) : Whether or not to print the probabilities.
+    
+    Return: Boolean determining whether the model thinks the action is happening or not. 
     """
     
-    # TODO : Fix this 
     # Convert video to list of numpy arrays of each frame
     video_list = vid_to_np(video)
     video_list = subsample_frames(video_list) # Subsample or trim the frames to match the expected number (32)
@@ -66,26 +69,32 @@ def inference(model, processor, video, action_desc, threshold):
         logits_per_video = outputs.logits_per_video  # Similarity score between video and text
         probs = logits_per_video.softmax(dim=1)  # Convert logits to probabilities
 
-    # Output the similarity score
-    similarity_score = probs[0][0].item()
-    print(f"Similarity Score: {similarity_score:.4f}")
+    # Similarity score for action in question
+    similarity_score = probs[0][idx].item()
+    # Most likely text description
+    max_val = max(probs[0])
+    
+    # Print scores
+    if print_scores:
+        print(probs)
+        print(f"Similarity Score: {similarity_score:.4f}")
+        print(max_val)
 
-    # Define a threshold to determine if the action is present
-    if similarity_score > threshold:
+
+    # Return if the action is present
+    if similarity_score == max_val:
         return True
     else:
         return False
 
-
 def main():
-    processor = AutoProcessor.from_pretrained("microsoft/xclip-base-patch32")
-    model = AutoModel.from_pretrained("microsoft/xclip-base-patch32")
-    video = './clips/Survivor-Scene-008.mp4'
-    desc = "An Australian beauty pageant."
-    threshold = 0.75
-    print(inference(model, processor, video, desc, threshold))
-
-
+    processor = AutoProcessor.from_pretrained("microsoft/xclip-base-patch16-ucf-16-shot")
+    model = AutoModel.from_pretrained("microsoft/xclip-base-patch16-ucf-16-shot")
+    video = './clips/Survivor-Scene-008.mp4' # a 6 sec clip of an Austrilan beauty pagent from austrailan survivor
+    #desc = ["An Austrilan beauty pagent.", "NOT an Austrilan beauty pagent"] # Correct
+    #desc = ["A baseball game", "NOT a baseball game"] # Corret
+    desc = ["A concert", "NOT a concert"] # ncorrect
+    print(inference(model, processor, video, desc, 0, True))
 
 if __name__ == "__main__":
     main()
